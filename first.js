2b45a0d64326feb014a48d03da529646d9362949
Chul <scahn.x@gmail.com>
2014-09-02 21:58:48 -0400

    Added Settings
    In settings, lb and kg can be selected and will adjust the slider, and
    plates used for determine the sides and divs that show weight.

--- first.js
+++ first.js
@@ -24,8 +24,11 @@ var RowStandards = [[80,145,175,240,320], [85,155,190,260,345],[90,170,205,280,3
 var PowerCleanStandards= [[55,105,125,175,205], [ 60,110,135,185,225], [65,120,150,200,240], [75,135,165,225,265] ,
  [80,145,180,245,290], [85,160,195,265,310], [90,165,205,280,325], [95,175,215,295,345], [100,185,225,305,355],
  [105,190,230,315,365], [110,195,235,320,375] , [115,200,240,330,385]];
+
+var slider = [[45,500,5],[20,250,2.5]];
 var standard = ["Untrained", "Novice", "Intermediate", "Advanced", "Elite"];
 var exercises = ["Squat", "Bench Press", "Deadlift", "Overhead Press", "Pendlay Rows", "Power Cleans"];
+var plates = [[45,35,25,10,5,2.5] ,[20,15,10,5,2.5,1]];
 var lbkg = ["Pounds", "Kg"];
 
 function setCookie(cname,cvalue,exdays) {
@@ -194,23 +197,51 @@ $(document).ready(function () {
 		setCookie("bodyweight", $(this).val(), 30);
 	});
 
+	//when page loads, determine if in lb or kg, and check appropriate box and set plates
+	var weightSystem = getCookie('lbkg');
+	var numPlates = $('#plates input').length;
+	if (weightSystem == "1") {
+		$('#radio-choice-h-2b').prop('checked',"checked");
+		$('#radio-choice-h-2a').prop('checked',"");
+			
+	}
+	if (weightSystem == "0") {
+		$('#radio-choice-h-2b').prop('checked',"");
+		$('#radio-choice-h-2a').prop('checked',"checked");
+			
+	}
+	for (i=0; i<numPlates+1; i++) {
+		// plateWeight = parseFloat($('label[for="plate'+i+'"]').text());
+		$('label[for="plate'+(i+1)+'"]').text(plates[weightSystem][i]);
+	}
+
+	//handler for lbkg to set weights
+	$('#weightSystem').change(function () {
+		for (i=0; i<numPlates+1; i++) {
+		$('label[for="plate'+(i+1)+'"]').text(plates[getCookie('lbkg')][i]);
+		$('.slider').attr('min' ,slider[getCookie('lbkg')][0]);
+		$('.slider').attr('max' ,slider[getCookie('lbkg')][1]);
+		$('.slider').attr('step' ,slider[getCookie('lbkg')][2]);
+	}
+	})
+
 	//each exercise, create page
 	$('.Exercise').each(function () {
 		var exerciseName = $(this).attr('id');
 		$('<header data-theme="b" data-role="header"><h1>'+$(this).children("h1").text()+'</h1><a href="#" class ="ui-btn-left ui-btn ui-btn-inline ui-mini ui-corner-all ui-icon-back ui-btn-icon-left" data-rel="back">Back</a><a href="#" class ="ui-btn-right ui-btn ui-btn-inline ui-mini ui-corner-all ui-icon-gear ui-btn-icon-left">Settings</a></header>').prependTo($(this));
-		$(this).append('<h2 class="bw"> Your bodyweight is '+bodyweight+' pounds <h3 class="wc"> You are in the ' + weightClass[wc] + ' lbs weight class');
+		$(this).append('<h2 class="bw"> Your bodyweight is '+bodyweight+' '+lbkg[getCookie('lbkg')]+' <h3 class="wc"> You are in the ' + weightClass[wc]+ ' '+ lbkg[getCookie('lbkg')]+' weight class');
 		// $(this).append('<h3 class="wc"> You are in the ' + weightClass[wc] + ' lbs weight class');
 
 		//if weight was set before, set the input value
-		var weight= 50;
+		var weight=40;
 		if (getCookie(exerciseName) != ""){
 			weight = parseInt( getCookie(exerciseName) );
 		}
 		//add working weight slider, working sets and 1rm
-		$('<p></p>Working Weight<input id='+exerciseName+'Weight type="range" value='+weight+' min="45" max="500" step="5" /><div id="Sets"><div>45x5x2 (Bar)</div><div id='+exerciseName+'warmup1></div><div id='+exerciseName+'warmup2></div><div id='+exerciseName+'warmup3></div><div class="work" id='+exerciseName+'warmup4></div></div><br /><div id ='+exerciseName+'max></div><p></p><div id='+exerciseName+'Standard></div>').appendTo($(this));
-		
+		$('<p></p>Working Weight<input class="slider" id='+exerciseName+'Weight type="range" value='+weight+' min='+slider[getCookie('lbkg')][0]+' max='+slider[getCookie('lbkg')][1]+' step='+slider[getCookie('lbkg')][2]+' /><div id="Sets"><div>45x5x2 (Bar)</div><div id='+exerciseName+'warmup1></div><div id='+exerciseName+'warmup2></div><div id='+exerciseName+'warmup3></div><div class="work" id='+exerciseName+'warmup4></div></div><br /><div id ='+exerciseName+'max></div><p></p><div id='+exerciseName+'Standard></div>').appendTo($(this));
+		//set warmup divs
 		calculateWarmups(exerciseName, weight);
-
+		//weight standards table
 		$('<div data-role="collapsible"><h3>Strength Standards</h3><div id='+exerciseName+'Tab class="tab"><table align="center" style="width: 30%; height: 160px;" class="auto-style2"><tr><th></th><th colspan="5"></th></tr><tr><td>Body Weight</td><td>Untrained</td><td>Novice</td><td>Intermediate</td><td>Advanced</td><td>Elite</td></tr><tr><td></td><td></td><td></td><td></td><td></td><td></td></tr><tr><td></td><td></td><td></td><td></td><td></td><td></td></tr><tr><td style="height: 26px"></td><td style="height: 26px"></td><td style="height: 26px"></td><td style="height: 26px"></td><td style="height: 26px"></td><td style="height: 26px"></td></tr><tr><td></td><td></td><td></td><td></td><td></td><td></td></tr><tr><td></td><td></td><td></td><td></td><td></td><td></td></tr><tr><td></td><td></td><td></td><td></td><td></td><td></td></tr><tr><td></td><td></td><td></td><td></td><td></td><td></td></tr><tr><td></td><td></td><td></td><td></td><td></td><td></td></tr><tr><td></td><td></td><td></td><td></td><td></td><td></td></tr><tr><td></td><td></td><td></td><td></td><td></td><td></td></tr><tr><td></td><td></td><td></td><td></td><td></td><td></td></tr><tr><td></td><td></td><td></td><td></td><td></td><td></td></tr></table></div></div>').appendTo($(this));
 
 		//replace with h1
@@ -236,11 +267,11 @@ $(document).ready(function () {
 	$(document).on('pageshow', ".Exercise", function(){
 		bodyweight = getCookie('bodyweight');
 		wc = getWeightClass(bodyweight);
-		$('.bw').html('Your bodyweight is '+bodyweight+' pounds');
-		$('.wc').html(' You are in the ' + weightClass[wc] + ' lbs weight class');
+		$('.bw').html('Your bodyweight is '+bodyweight+' '+lbkg[getCookie('lbkg')]+'');
+		$('.wc').html(' You are in the ' + weightClass[wc]+ ' '+ lbkg[getCookie('lbkg')]+' weight class');
 		var exerciseName = $(this).attr('id');
 
-		$('#'+exerciseName+'Weight' ).change(function (e) {
+		$('#'+exerciseName+'Weight').change(function (e) {
 			var weight = $(this).val();
 			if ($.isNumeric(weight))
 			{
