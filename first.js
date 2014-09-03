a1aaa8a0df867896da9ec1a7073ed63f72c1978b
Chul <scahn.x@gmail.com>
2014-09-02 23:37:25 -0400

    Added most kg compatibility
    added weight classes and standards in kg, and shows correct weight,
    weight class and strength standards table

--- first.js
+++ first.js
@@ -1,35 +1,63 @@
 //add Women and kg
 
-
+//0 for lb 1 for kg
 //Men + Pounds
-var weightClass = [114,123,132,148,165,181,198,220,242,275,319,"320+"];
-//index from weightClass determines standard
+var weightClass = [[114,123,132,148,165,181,198,220,242,275,319,"320+"], [52,56,60,67,75,82,90,100,110,125,145,"145+"]];
 
-var OHPStandards = [[55,75,90,110,130], [60,80,100,115,140], [65,85,105,125,150], [70,95,120,140,170] ,
+//index from weightClass determines standard
+//[determines lb or kg][determines which weight class][determines which strength standard]
+var OHPStandards = [
+[[55,75,90,110,130], [60,80,100,115,140], [65,85,105,125,150], [70,95,120,140,170] ,
  [75,100,130,155,190], [80,110,140,165,220], [85,115,145,175,235], [90,120,155,185,255], [95,125,160,190,265],
- [95,130,165,195,275], [100,135,170,200,280] , [100,140,175,205,285]];
-var SquatStandards = [[80,145,175,240,320], [85,155,190,260,345],[90,170,205,280,370], [100,190,230,315,410] ,
+ [95,130,165,195,275], [100,135,170,200,280] , [100,140,175,205,285]]
+,
+[[22.5,32.5,40.0,50.0,60.0],[25.0,35.0,45.0,52.5,65.0],[27.5,37.5,47.5,57.5,70.0],[30.0,42.5,55.0,62.5,77.5],[32.5,45.0,57.5,70.0,85.0],[35.0,50.0,62.5,75.0,100.0],[37.5,52.5,65.0,77.5,105.0],[40.0,55.0,70.0,82.5,115.0],[42.5,57.5,72.5,85.0,120.0],[42.5,60.0,75.0,87.5,122.5],[45.0,60.0,75.0,90.0,125.0],[45.0,62.5,77.5,92.5,130.0]]
+];
+
+
+var SquatStandards = [[[80,145,175,240,320], [85,155,190,260,345],[90,170,205,280,370], [100,190,230,315,410] ,
  [110,205,250,340,445],[120,220,270,370,480], [125,230,285,390,505], [130,245,300,410,530], [135,255,310,425,550],
- [140,260,320,435,570], [145,270,325,445,580], [150,275,330,455,595]];
-var DeadliftStandards = [[95,180,205,300,385], [105,195,220,320,415],[115,210,240,340,440], [125,235,270,380,480] ,
+ [140,260,320,435,570], [145,270,325,445,580], [150,275,330,455,595]]
+
+,
+
+[[35.0,65.0,80.0,107.5,145.0],[37.5,70.0,87.5,117.5,157.5],[40.0,77.5,92.5,127.5,167.5],[45.0,85.0,105.0,142.5,185.0],[50.0,92.5,112.5,155.0,202.5],[55.0,100.0,122.5,167.5,217.5],[57.5,105.0,130.0,177.5,230.0],[60.0,110.0,135.0,185.0,240.0],[62.5,115.0,140.0,192.5,250.0],[65.0,117.5,145.0,197.5,257.5],[67.5,122.5,147.5,202.5,262.5],[70.0,125.0,150.0,207.5,270.0]]]
+ ;
+var DeadliftStandards = [[[95,180,205,300,385], [105,195,220,320,415],[115,210,240,340,440], [125,235,270,380,480] ,
  [135,255,295,410,520],[150,275,315,440,550], [155,290,335,460,565], [165,305,350,480,585], [170,320,365,490,595],
- [175,325,375,500,600], [180,335,380,505,610], [185,340,390,510,615]];
-var BenchStandards=[[85,110,130,180,220], [90,115,140,195,240],[100,125,155,210,260], [110,140,170,235,290] ,
+ [175,325,375,500,600], [180,335,380,505,610], [185,340,390,510,615]]
+,
+[[42.5,82.5,92.5,135.0,175.0],[47.5,87.5,100.0,145.0,187.5],[50.0,95.0,110.0,155.0,200.0],[57.5,107.5,122.5,172.5,217.5],[62.5,115.0,135.0,185.0,235.0 ],[67.5,125.0,142.5,200.0,250.0],[70.0,132.5,152.5,207.5,257.5],[75.0,137.5,160.0,217.5,265.0],[77.5,145.0,165.0,222.5,270.0],[80.0,147.5,170.0,227.5,272.5],[82.5,152.5,172.5,230.0,277.5],[85.0,155.0,177.5,232.5,280.0]]];
+
+var BenchStandards=[[[85,110,130,180,220], [90,115,140,195,240],[100,125,155,210,260], [110,140,170,235,290] ,
  [120,150,185,255,320],[130,165,200,275,345], [135,175,215,290,360], [140,185,225,305,380], [145,190,230,315,395],
- [150,195,240,325,405], [155,200,245,335,415], [160,205,250,340,425]];
+ [150,195,240,325,405], [155,200,245,335,415], [160,205,250,340,425]] 
+ ,
+ [[37.5,50.0,60.0,82.5,100.0],[40.0,52.5,62.5,90.0,110.0],[45.0,57.5,70.0,95.0,117.5],[50.0,65.0,77.5,107.5,132.5],[55.0,70.0,85.0,115.0,145.0],[60.0,75.0,90.0,125.0,157.5],[62.5,80.0,97.5,132.5,162.5],[62.5,82.5,102.5,137.5,172.5],[65.0,85.0,105.0,142.5,180.0],[67.5,87.5,107.5,147.5,185.0],[70.0,90.0,112.5,152.5,190.0],[72.5,92.5,115.0,155.0,192.5]]
+];
  //no row standards
-var RowStandards = [[80,145,175,240,320], [85,155,190,260,345],[90,170,205,280,370], [100,190,230,315,410] ,
- [110,205,250,340,445],[120,220,270,370,480], [125,230,285,390,505], [130,245,300,410,530], [135,255,310,425,550],
- [140,260,320,435,570], [145,270,325,445,580], [150,275,330,455,595]];
-var PowerCleanStandards= [[55,105,125,175,205], [ 60,110,135,185,225], [65,120,150,200,240], [75,135,165,225,265] ,
+var RowStandards = [[[85,110,130,180,220], [90,115,140,195,240],[100,125,155,210,260], [110,140,170,235,290] ,
+ [120,150,185,255,320],[130,165,200,275,345], [135,175,215,290,360], [140,185,225,305,380], [145,190,230,315,395],
+ [150,195,240,325,405], [155,200,245,335,415], [160,205,250,340,425]] 
+ ,
+ [[37.5,50.0,60.0,82.5,100.0],[40.0,52.5,62.5,90.0,110.0],[45.0,57.5,70.0,95.0,117.5],[50.0,65.0,77.5,107.5,132.5],[55.0,70.0,85.0,115.0,145.0],[60.0,75.0,90.0,125.0,157.5],[62.5,80.0,97.5,132.5,162.5],[62.5,82.5,102.5,137.5,172.5],[65.0,85.0,105.0,142.5,180.0],[67.5,87.5,107.5,147.5,185.0],[70.0,90.0,112.5,152.5,190.0],[72.5,92.5,115.0,155.0,192.5]]
+];
+var PowerCleanStandards= [[[55,105,125,175,205], [ 60,110,135,185,225], [65,120,150,200,240], [75,135,165,225,265] ,
  [80,145,180,245,290], [85,160,195,265,310], [90,165,205,280,325], [95,175,215,295,345], [100,185,225,305,355],
- [105,190,230,315,365], [110,195,235,320,375] , [115,200,240,330,385]];
+ [105,190,230,315,365], [110,195,235,320,375] , [115,200,240,330,385]]
+
+,
+
+[[25.0,47.5,57.5,80.0,92.5],[27.5,50.0,62.5,85.0,102.5],[30.0,55.0,67.5,90.0,110.0],[35.0,60.0,75.0,102.5,120.0],[37.5,65.0,82.5,112.5,132.5],[37.5,72.5,87.5,120.0,140.0],[40.0,75.0,92.5,127.5,147.5],[42.5,80.0,97.5,135.0,155.0],[45.0,82.5,102.5,140.0,160.0],[47.5,85.0,105.0,142.5,165.0],[50.0,87.5,107.5,145.0,170.0],[52.5,90.0,110.0,150.0,175.0]]]
+
+
+ ;
 
 var slider = [[45,500,5],[20,250,2.5]];
 var standard = ["Untrained", "Novice", "Intermediate", "Advanced", "Elite"];
 var exercises = ["Squat", "Bench Press", "Deadlift", "Overhead Press", "Pendlay Rows", "Power Cleans"];
 var plates = [[45,35,25,10,5,2.5] ,[20,15,10,5,2.5,1]];
-var lbkg = ["Pounds", "Kg"];
+var lbkg = ["lb", "kg"];
 
 function setCookie(cname,cvalue,exdays) {
     var d = new Date();
@@ -125,9 +153,9 @@ function numToPlate(data) {
 
 function getWeightClass(data) {
 	var w;
-	for (i=0; i<weightClass.length; i++) {
+	for (i=0; i<weightClass[getCookie('lbkg')].length; i++) {
 
-		if (weightClass[i] > data) {
+		if (weightClass[getCookie('lbkg')][i] > data) {
 			if (i==0){
 				w=0;
 			}
@@ -171,8 +199,8 @@ function pickWorkout(exercise) {
 function weightStandard(exercise, wc, oneRM) {
 	workout = pickWorkout(exercise);
 	var w;
-	for (i=0; i<workout[wc].length; i++) {
-		if (workout[wc][i] > oneRM){
+	for (i=0; i<workout[getCookie('lbkg')][wc].length; i++) {
+		if (workout[getCookie('lbkg')][wc][i] > oneRM){
 			if (i==0){
 				w = 0;
 			}
@@ -195,6 +223,7 @@ $(document).ready(function () {
 	var wc = getWeightClass(bodyweight);
 	$('#bodyweight').change(function (e) {
 		setCookie("bodyweight", $(this).val(), 30);
+		wc = getWeightClass($(this).val());
 	});
 
 	//when page loads, determine if in lb or kg, and check appropriate box and set plates
@@ -207,16 +236,13 @@ $(document).ready(function () {
 	var numPlates = $('#plates input').length;
 	if (weightSystem == "1") {
 		$('#radio-choice-h-2b').prop('checked',"checked");
-		$('#radio-choice-h-2a').prop('checked',"");
-			
+		$('#radio-choice-h-2a').prop('checked',"");	
 	}
 	if (weightSystem == "0") {
 		$('#radio-choice-h-2b').prop('checked',"");
-		$('#radio-choice-h-2a').prop('checked',"checked");
-			
+		$('#radio-choice-h-2a').prop('checked',"checked");		
 	}
 	for (i=0; i<numPlates+1; i++) {
-		// plateWeight = parseFloat($('label[for="plate'+i+'"]').text());
 		$('label[for="plate'+(i+1)+'"]').text(plates[weightSystem][i]);
 	}
 
@@ -234,7 +260,7 @@ $(document).ready(function () {
 	$('.Exercise').each(function () {
 		var exerciseName = $(this).attr('id');
 		$('<header data-theme="b" data-role="header"><h1>'+$(this).children("h1").text()+'</h1><a href="#" class ="ui-btn-left ui-btn ui-btn-inline ui-mini ui-corner-all ui-icon-back ui-btn-icon-left" data-rel="back">Back</a><a href="#" class ="ui-btn-right ui-btn ui-btn-inline ui-mini ui-corner-all ui-icon-gear ui-btn-icon-left">Settings</a></header>').prependTo($(this));
-		$(this).append('<h2 class="bw"> Your bodyweight is '+bodyweight+' '+lbkg[getCookie('lbkg')]+' <h3 class="wc"> You are in the ' + weightClass[wc]+ ' '+ lbkg[getCookie('lbkg')]+' weight class');
+		$(this).append('<h2 class="bw"> Your bodyweight is '+bodyweight+' '+lbkg[getCookie('lbkg')]+' <h3 class="wc"> You are in the ' + weightClass[getCookie('lbkg')][wc]+ ' '+ lbkg[getCookie('lbkg')]+' weight class');
 		// $(this).append('<h3 class="wc"> You are in the ' + weightClass[wc] + ' lbs weight class');
 
 		//if weight was set before, set the input value
@@ -250,17 +276,17 @@ $(document).ready(function () {
 		$('<div data-role="collapsible"><h3>Strength Standards</h3><div id='+exerciseName+'Tab class="tab"><table align="center" style="width: 30%; height: 160px;" class="auto-style2"><tr><th></th><th colspan="5"></th></tr><tr><td>Body Weight</td><td>Untrained</td><td>Novice</td><td>Intermediate</td><td>Advanced</td><td>Elite</td></tr><tr><td></td><td></td><td></td><td></td><td></td><td></td></tr><tr><td></td><td></td><td></td><td></td><td></td><td></td></tr><tr><td style="height: 26px"></td><td style="height: 26px"></td><td style="height: 26px"></td><td style="height: 26px"></td><td style="height: 26px"></td><td style="height: 26px"></td></tr><tr><td></td><td></td><td></td><td></td><td></td><td></td></tr><tr><td></td><td></td><td></td><td></td><td></td><td></td></tr><tr><td></td><td></td><td></td><td></td><td></td><td></td></tr><tr><td></td><td></td><td></td><td></td><td></td><td></td></tr><tr><td></td><td></td><td></td><td></td><td></td><td></td></tr><tr><td></td><td></td><td></td><td></td><td></td><td></td></tr><tr><td></td><td></td><td></td><td></td><td></td><td></td></tr><tr><td></td><td></td><td></td><td></td><td></td><td></td></tr><tr><td></td><td></td><td></td><td></td><td></td><td></td></tr></table></div></div>').appendTo($(this));
 
 		//replace with h1
-		$('.tab th:nth-child(1)').html(lbkg[0]);
+		$('.tab th:nth-child(1)').html(lbkg[getCookie('lbkg')]);
 		$('#'+exerciseName+'Tab th:nth-child(2)').html($(this).children("h1").text());
 		var rows = $('#'+exerciseName+'Tab td:nth-child(1)').length;
 
 		for (i=0; i<rows-1; i++) {
 			//writes the weight classes
-			$('.tab tr:nth-child('+(i+3)+') td:nth-child(1)').html(weightClass[i]);
+			$('.tab tr:nth-child('+(i+3)+') td:nth-child(1)').html(weightClass[getCookie('lbkg')][i]);
 			$('.tab tr:nth-child('+(i+3)+') td:nth-child(1)').css("width", "20%");
 			var columns = $('#'+exerciseName+'Tab td').length
 			for (j=0; j<columns-1; j++) {
-				$('#'+exerciseName+'Tab tr:nth-child('+(i+3)+') td:nth-child('+(j+2)+'').html(pickWorkout(exerciseName)[i][j]);
+				$('#'+exerciseName+'Tab tr:nth-child('+(i+3)+') td:nth-child('+(j+2)+'').html(pickWorkout(exerciseName)[getCookie('lbkg')][i][j]);
 				$('.tab tr:nth-child('+(i+3)+') td:nth-child('+(j+2)+'').css("width", "16%");
 			}
 		}
@@ -273,7 +299,7 @@ $(document).ready(function () {
 		bodyweight = getCookie('bodyweight');
 		wc = getWeightClass(bodyweight);
 		$('.bw').html('Your bodyweight is '+bodyweight+' '+lbkg[getCookie('lbkg')]+'');
-		$('.wc').html(' You are in the ' + weightClass[wc]+ ' '+ lbkg[getCookie('lbkg')]+' weight class');
+		$('.wc').html(' You are in the ' + weightClass[getCookie('lbkg')][wc]+ ' '+ lbkg[getCookie('lbkg')]+' weight class');
 		var exerciseName = $(this).attr('id');
 
 		$('#'+exerciseName+'Weight').change(function (e) {
